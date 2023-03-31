283a801cbf6751815469f1ad8c558d4d79abba43
Lynxsole <john1880@outlook.fr>
2023-03-31 16:37:49 +0200

    modified:   assets/css/style.css 	modified:   assets/php/newElt.php

--- assets/php/newElt.php
+++ assets/php/newElt.php
@@ -1,5 +1,19 @@
 <?php
+    $accountName = ['lsacedit', 'lynx'];
     
+    $accountContent = [
+        $accountElt = [
+        'title' => $accountName[0],
+        'enabled' => true,
+    
+    ],
+
+    $accountElt = [
+        'title' => $accountName[1],
+        'enabled' => true,
+    
+    ],
+    ];
 
 
 ?>
