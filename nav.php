a1604b60df400c5eace44b796bc47786d4ed82df
lynxsole <john1880@outlook.fr>
2023-04-01 22:47:05 +0200

    modified:   nav.php

--- nav.php
+++ nav.php
@@ -5,7 +5,7 @@
             <li class="navBtn"><a href="#page2"><P class="nvt">Account </P></a></li>
             <li class="navBtn"><a href="#page3"><P class="nvt">Creation </p></a></li>
             <li class="navBtn"><a href="#"><P class="nvt">Autre</p></a></li>
-            <li class="navBtn" id="screenCount"><P style="display: inline-flex">membre :<p id="counter">0</p></p></li>
+            <li class="navBtn" id="screenCount"><P style="display: inline-flex">membre[ <span id="userCounter">0</span>]</p></li>
         </ul>
     </div>
 </nav>
