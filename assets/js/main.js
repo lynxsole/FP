ac0e2548f00e03a0ce58bfa99bd4356f0df9d776
lynxsole <john1880@outlook.fr>
2023-04-01 22:44:08 +0200

    ajout d'un conteur de membre

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ function checkBox() {
     if(document.getElementById('check').checked == true)
     {
        console.log("bip bop");
+       document.getElementById('error').style.display = "none";
     }else{
         console.log("bop bop bop");
         document.getElementById('error').style.display = "block";
