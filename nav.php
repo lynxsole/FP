6f89ef22d1708b8ed7d64d14d7abb2c09d1a947b
lynxsole <john1880@outlook.fr>
2023-04-04 00:02:10 +0200

    new file:   Ressource.php 	new file:   assets/css/ressource.css 	modified:   assets/css/style.css 	modified:   nav.php

--- nav.php
+++ nav.php
@@ -4,9 +4,10 @@
     <input id="hideBtn" style="width: 50px;  border-style: none; background-color: #282b30; font-family: 'Ubuntu', sans-serif; color: white;" type="button" onclick="showPhoneBtn()" value="menu">
     <div id="phoneNav" class="content">
         <ul class="navText">
-            <li class="navBtn"><a href="#page2"><P>Account </P></a></li>
+            <li class="navBtn"><a href="index.php"><P>Home </P></a></li>
             <li class="navBtn"><a href="#page3"><P>Creation </p></a></li>
             <li class="navBtn"><a href="#"><P>Autre</p></a></li>
+            <li class="navBtn"><a href="Ressource.php"><P>Ressource</p></a></li>
             <li class="navBtn" id="screenCount"><P style="display: inline-flex">membre[ <span id="userCounter">0</span>] <img style="height: 40px; width: 40px; margin-left: 15px; margin-top: -10px;" src="assets/img/icon/people.svg" alt=""> </p></li>
             <li class="navBtn"><P><input id="hideBtn" style="width: 50px;  border-style: none; background-color: #36393e; font-family: 'Ubuntu', sans-serif; color: white;" type="button" onclick="hidePhoneBtn()" value="menu"></p></li> 
         </ul>
